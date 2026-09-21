b580b7e60d5c7fa0aa54de569b97a40d0c27f26f
Luciano Crouzeilles <lrcrouzeilles@hotmail.com>
2026-09-21 17:30:24 -0300

    Refactor to include api-gateway module within the application

---
+++ api-gateway/src/main/java/com/finflow/apigateway/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.finflow.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/actuator/health").permitAll()
+                        .requestMatchers("/transfers/**")
+                        .hasAnyRole("USER", "ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 ->
+                        oauth2.jwt(jwt ->
+                                jwt.jwtAuthenticationConverter(new KeycloakRoleConverter())
+                        )
+                );
+
+        return http.build();
+    }
+}
